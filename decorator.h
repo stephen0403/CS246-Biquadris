143adcd781220011477660621c1b139d84cefa5a
root <root@ThinkPanda.localdomain>
2023-07-19 14:51:46 -0400

    decorator for special actions

---
+++ decorator.h
@@ -0,0 +1,12 @@
+#ifndef DECORATOR_H
+#define DECORATOR_H
+
+#include absBoard.h 
+
+class Decorator: public absBoard {
+  protected:
+    absBoard* component;
+  public:
+    Decorator(absBoard* board);
+    virtual ~Decorator(); // Note: destructor does not do anything. Client must 
+};                        // deallocate component through the use of smart pointers
