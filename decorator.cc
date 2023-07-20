1191ca827294a2aaece863c7ad7665056d146e7a
stephen0403 <stephenim83@yahoo.ca>
2023-07-20 13:03:43 -0400

    Update decorator.cc

--- decorator.cc
+++ decorator.cc
@@ -1,6 +1,7 @@
 #include "decorator.h"
 
-Decorator::Decorator(absBoard* board): component{board} {}
+Decorator::Decorator(absBoard* board, int row, int col, char character)
+  : board{board}, row{row}, col{col}, character{character} {}
 
 Decorator::~Decorator() {
 
