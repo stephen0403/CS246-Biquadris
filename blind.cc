0e4d3a55c64ca2a5ab1fe8ea699017b7fdd39853
stephen0403 <stephenim83@yahoo.ca>
2023-07-20 14:58:45 -0400

    Update blind.cc

--- blind.cc
+++ blind.cc
@@ -6,10 +6,18 @@ blind::blind(char character, absBoard *board)
   : Decorator{board, character} {}
 
 char blind::tileAt(int row, int col) {
-  char under = board->tileAt(row, col);
-  bounds = row >= 3 && row <=  // put which tiles should be covered
 
   // something along the lines of if in bounds of where it should be covered with ???, then return character
   // else return under
+  
+  char under = board->tileAt(row, col);
+  rowBounds = row >= 2 && row <= 8; // put which tiles should be covered // are we starting 0 index or 1 index?
+  colBounds = col >= 2 && <= 11;
 
+  if (rowBounds && colBounds) {
+    return character;
+  }
+  else {
+    return under;
+  }
 };
