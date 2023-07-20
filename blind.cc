86183355aa845d1ed30c418596d185a475122b3c
stephen0403 <stephenim83@yahoo.ca>
2023-07-20 14:55:57 -0400

    Update blind.cc

--- blind.cc
+++ blind.cc
@@ -2,12 +2,12 @@
 #include "absboard.h"
 #include <iostream>
 
-blind::blind(int row, int col, char character, absBoard *board)
-  : Decorator{board, row, col, character} {}
+blind::blind(char character, absBoard *board)
+  : Decorator{board, character} {}
 
 char blind::tileAt(int row, int col) {
   char under = board->tileAt(row, col);
-  bounds = '' // put which tiles should be covered
+  bounds = row >= 3 && row <=  // put which tiles should be covered
 
   // something along the lines of if in bounds of where it should be covered with ???, then return character
   // else return under
