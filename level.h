677b47d783ddfab7c4a59a7f434345030cbbb1d9
Soham Basu <sohambasu963@gmail.com>
2023-07-20 12:48:42 -0400

    added level 0 class

--- level.h
+++ level.h
@@ -1,12 +1,16 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
 #include "block.h"
 #include "board.h"
 
 class Level {
-    int curLevel;
-
-protected:
     Board *board;
+
 public:
-    Level(Board *board): board{board} {};
+    Level(Board *board);
+    Block *createBlock(char type);
     virtual Block *generateBlock() = 0;
 };
+
+#endif
