4411f8a168ba840755ed00ca872b6feec8d90ecb
stephen0403 <stephenim83@yahoo.ca>
2023-07-20 13:05:33 -0400

    Update blind.cc

--- blind.cc
+++ blind.cc
@@ -5,7 +5,7 @@
 blind::blind(int row, int col, char character, absBoard *board)
   : Decorator{board, row, col, character} {}
 
-char blind::tileAt(int row, int col, char character) {
+char blind::tileAt(int row, int col) {
 
 
 };
