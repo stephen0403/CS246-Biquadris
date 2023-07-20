5a63ce1cd7cb8dc73880e9dfd0481c892518aedf
stephen0403 <stephenim83@yahoo.ca>
2023-07-20 13:07:16 -0400

    Update blind.cc

--- blind.cc
+++ blind.cc
@@ -6,6 +6,7 @@ blind::blind(int row, int col, char character, absBoard *board)
   : Decorator{board, row, col, character} {}
 
 char blind::tileAt(int row, int col) {
-
+  char under = board->tileAt(row, col);
+  bounds = '' // put which tiles should be covered
 
 };
