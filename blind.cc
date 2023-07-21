cc8f55eda41c85096a08e61768a6987b88900d2a
root <root@ThinkPanda.localdomain>
2023-07-20 21:04:59 -0400

    fix

--- blind.cc
+++ blind.cc
@@ -1,17 +1,32 @@
 #include "blind.h"
+#include "absboard.h"
+#include <iostream>
 
 static const int COLUMN_LEFT = 3;
 static const int COLUMN_RIGHT = 9;
 static const int ROW_TOP = 3;
 static const int ROW_BOTTOM = 12;
 
-Blind::Blind(absBoard* board): Decorator {board} {}
+blind::blind(char character, absBoard *board, )
+  : Decorator{board, character} {}
 
-char Blind::tileAt(int row, int col) {
-    if (!blockDropped && row <= ROW_BOTTOM && row >= ROW_TOP
-        && col <= COLUMN_RIGHT && col >= COLUMN_LEFT) return '?';
+char blind::tileAt(int row, int col) {
+
+  // something along the lines of if in bounds of where it should be covered with ???, then return character
+  // else return under
+  
+  char under = board->tileAt(row, col);
+  rowBounds = row >= 2 && row <= 8; // put which tiles should be covered // are we starting 0 index or 1 index?
+  colBounds = col >= 2 && <= 11;
+
+  if (rowBounds && colBounds && !blockDropped) {
+    return character;
+  }
+  else {
+    return under;
+  }
 }
 
-void Blind::setTrue() {
+void Blind::setBlockDroppedTrue() {
     blockDropped = true;
 }
