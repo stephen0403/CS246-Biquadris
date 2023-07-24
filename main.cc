ecc4c0f46066286fefc37a6eca0891801e79f94b
danlu <daniel.lu2@uwaterloo.ca>
2023-07-24 12:01:22 -0400

    new main

--- main.cc
+++ main.cc
@@ -9,6 +9,17 @@ int main(int argc, char *argv[]) {
   std::string file2{"sequence2.txt"};
   int level = 0;
   unsigned int seed = 0;
+  int currentPlayer = 0;                                        // current player
+  
+  std::unique_ptr<absBoard> board1 = std::make_unique<Board> (); // board1
+  std::unique_ptr<absBoard> board2 = std::make_unique<Board> (); // board2
+  std::vector<absBoard*> boards {board1.get(), board2.get()};
+  std::vector<Level> levels{Level0(boards.at(currentPlayer)),
+                            Level1(boards.at(currentPlayer)),
+                            Level2(boards.at(currentPlayer)),
+                            Level3(boards.at(currentPlayer)),
+                            Level4(boards.at(currentPlayer))};
+
   for (int i = 1; i < argc; i += 2) {
     std::string arg{argv[i]};
     if (arg == "-text") {
@@ -24,12 +35,12 @@ int main(int argc, char *argv[]) {
       } else if (arg == "-seed") {
         iss >> seed;
       }
-    }
-  }
-  GamePlay game{file1, file2, 2, 2, seed};
-  TextDisplay td{&game};
-  std::string cmd;
-  td.display();
+    }  }
+
+  //GamePlay game{file1, file2, 2, 2, seed};
+  //TextDisplay td{&game};
+  //std::string cmd;
+  //td.display();
   std::cout << std::endl;
   while (std::cin >> cmd) {
     if (cmd == "d"/*rop"*/) {
