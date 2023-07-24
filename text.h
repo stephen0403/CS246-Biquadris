651bfe9ab4bd09aaa427ce1772598c5127a4f210
Alex Li <65035465+yook40@users.noreply.github.com>
2023-07-24 17:48:04 -0400

    Add files via upload

--- text.h
+++ text.h
@@ -20,3 +20,18 @@ class TextDisplay: public Observer {
 };
 
 #endif
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+#include <string>
+
+class GamePlay;
+
+class TextDisplay {
+  GamePlay *game;
+  void printHeader(std::string, int) const;
+  public:
+  TextDisplay(GamePlay *);
+  void display();
+};
+
+#endif
