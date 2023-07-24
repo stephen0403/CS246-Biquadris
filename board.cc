32d20c2ca214a62b8bc95cf68340fefcada49ca3
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 16:53:57 -0400

    Update board.cc

--- board.cc
+++ board.cc
@@ -2,7 +2,9 @@
 // #include <sstream>
 
 
-Board::Board(): numRows{18}, numCols{11}, board(numRows, std::vector<char>(numCols, ' ')) {}
+Board::Board(absBoard *board): numRows{18}, numCols{11}, board(board) {
+  shift(0, 0, false);
+}
 
 char Board::tileAt(int row, int col) { return board.at(row).at(col); }
 
@@ -16,46 +18,91 @@ char Board::getState() {
   return board->tileAt(row, col);
 }
 
-  //current = std::make_unique<JBlock>(JBlock(this));
-  //current->updatePos(0, 0);
-//}
-/*
-std::string Board::getScore() const {
-  std::ostringstream res;
-  res << " Score:";
-  int numSpaces = 5 - numDigits(score);
-  while (numSpaces) {
-    res << " ";
-    --numSpaces;
-  }
-  res << score;
-  return res.str();
-}
-
-
-
-void Board::move(int x, int y) {
-  current->updatePos(x, y);
-}
-
-void Board::rotate(bool clockwise) {
-  current->rotate(clockwise);
-  current->updatePos(0, 0);
-}
-
-void Board::updateCurrent() {
-  current = next;
-}
-
-
-
-void Board::updateScore() {
-  for (auto &row : board) {
-    if (isFull(row)) {
-      ++score;
+static bool isFull(const std::vector<char> &row) {
+  for (auto c : row) {
+    if (c == ' ') {
+      return false;
     }
   }
-  score += level;
+  return true;
 }
-*/
 
+
+bool Board::shift(int x, int y, bool drop, Block *block) {
+  // auto block = queue.front().get();
+  std::vector<std::vector<int>> &pos = block->pos;
+  for (int i = 0; i < 4; ++i) {
+    board.at(pos[i][0]).at(pos[i][1]) = ' ';
+  }
+  for (int i = 0; i < 4; ++i) {
+    if (pos[i][0] + y >= numRows || pos[i][1] + x < 0 || pos[i][1] + x >= numCols ||
+        board.at(pos[i][0] + y).at(pos[i][1] + x) != ' ') {
+      if (y && drop) {
+        dropped = true;
+        for (int i = 0; i < 4; ++i) {
+          if (block->pos[i][0] < 3) {
+            for (int i = 0; i < 4; ++i) {
+              block->pos[i][0] += y;
+              block->pos[i][1] += x;
+              board.at(pos[i][0]).at(pos[i][1]) = block->getType();
+            }
+            throw; // winner
+          }
+        }
+      }
+      for (int i = 0; i < 4; ++i) {
+        board.at(pos[i][0]).at(pos[i][1]) = block->getType();
+      }
+      return false;
+    }
+  }
+  block->lowerLeft[0] += y;
+  block->lowerLeft[1] += x;
+  for (int i = 0; i < 4; ++i) {
+    block->pos[i][0] += y;
+    block->pos[i][1] += x;
+    board.at(pos[i][0]).at(pos[i][1]) = block->getType();
+  }
+  return true;
+}
+
+
+
+
+// std::string Board::getScore() const {
+//   std::ostringstream res;
+//   res << " Score:";
+//   int numSpaces = 5 - numDigits(score);
+//   while (numSpaces) {
+//     res << " ";
+//     --numSpaces;
+//   }
+//   res << score;
+//   return res.str();
+// }
+
+
+
+// void Board::move(int x, int y) {
+//   current->updatePos(x, y);
+// }
+
+// void Board::rotate(bool clockwise) {
+//   current->rotate(clockwise);
+//   current->updatePos(0, 0);
+// }
+
+// void Board::updateCurrent() {
+//   current = next;
+// }
+
+
+
+// void Board::updateScore() {
+//   for (auto &row : board) {
+//     if (isFull(row)) {
+//       ++score;
+//     }
+//   }
+//   score += level;
+// }
