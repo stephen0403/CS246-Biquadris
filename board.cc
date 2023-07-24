5fc0eca3fe2ebde17a9994792715211ad2fc91b4
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 16:15:23 -0400

    Update board.cc

--- board.cc
+++ board.cc
@@ -16,45 +16,42 @@ char Board::getState() {
   return board->tileAt(row, col);
 }
 
-  //current = std::make_unique<JBlock>(JBlock(this));
-  //current->updatePos(0, 0);
-//}
-/*
-std::string Board::getScore() const {
-  std::ostringstream res;
-  res << " Score:";
-  int numSpaces = 5 - numDigits(score);
-  while (numSpaces) {
-    res << " ";
-    --numSpaces;
-  }
-  res << score;
-  return res.str();
-}
+
+// std::string Board::getScore() const {
+//   std::ostringstream res;
+//   res << " Score:";
+//   int numSpaces = 5 - numDigits(score);
+//   while (numSpaces) {
+//     res << " ";
+//     --numSpaces;
+//   }
+//   res << score;
+//   return res.str();
+// }
 
 
 
-void Board::move(int x, int y) {
-  current->updatePos(x, y);
-}
+// void Board::move(int x, int y) {
+//   current->updatePos(x, y);
+// }
 
-void Board::rotate(bool clockwise) {
-  current->rotate(clockwise);
-  current->updatePos(0, 0);
-}
+// void Board::rotate(bool clockwise) {
+//   current->rotate(clockwise);
+//   current->updatePos(0, 0);
+// }
 
-void Board::updateCurrent() {
-  current = next;
-}
+// void Board::updateCurrent() {
+//   current = next;
+// }
 
 
 
-void Board::updateScore() {
-  for (auto &row : board) {
-    if (isFull(row)) {
-      ++score;
-    }
-  }
-  score += level;
-}
-*/
+// void Board::updateScore() {
+//   for (auto &row : board) {
+//     if (isFull(row)) {
+//       ++score;
+//     }
+//   }
+//   score += level;
+// }
+
