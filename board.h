b40cef67ef4913f767f53e3c814a6f16a9b07a08
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 16:10:39 -0400

    Update board.h

--- board.h
+++ board.h
@@ -1,13 +1,22 @@
-#ifndef _BOARD_H_
+#ifndef _BOARD_H_ // concrete subject and concrete component class
 #define _BOARD_H_
 #include <vector>
+#include "absBoard.h"
+#include "subject.h"
 
-struct Board {
+class Board: public absBoard, public Subject {
   int numRows;
   int numCols;
-  Board();
-  std::vector<std::vector<char>> board;
-  char tileAt(int row, int col); // for the decorator, can also use for display
+  // std::vector<std::vector<char>> board;
+  absBoard *board;
+  bool blindActivated = false;
+  bool blockDroppedAfterBlind = false;
+  public:
+  explicit Board(absBoard *board): board{board} {}
+  char getState(int row, int col) const override;
+  char tileAt(int row, int col) override;
+  ~Board();
+  void render();
 };
 
 #endif
