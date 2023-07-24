fd6719fb94b01c0ee53f6902a19b98417732ce61
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 17:41:40 -0400

    Update display.h

--- display.h
+++ display.h
@@ -1,16 +1,16 @@
-#ifndef _DISPLAY_H_
-#define _DISPLAY_H_
-#include <iostream>
+// #ifndef _DISPLAY_H_
+// #define _DISPLAY_H_
+// #include <iostream>
 
-class Board;
+// class Board;
 
-class Display {
-  Board *bp1;
-  Board *bp2;
-  public:
-  Display(Board *, Board *);
-  Board *getBoard(int) const;
-};
+// class Display {
+//   Board *bp1;
+//   Board *bp2;
+//   public:
+//   Display(Board *, Board *);
+//   Board *getBoard(int) const;
+// };
 
-std::ostream &operator<<(std::ostream &, Display &);
-#endif
+// std::ostream &operator<<(std::ostream &, Display &);
+// #endif
