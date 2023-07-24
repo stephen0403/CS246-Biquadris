9aa138a776c70e6222a885d0e33e7a09af064b6c
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 15:22:47 -0400

    Update board.h

--- board.h
+++ board.h
@@ -12,7 +12,7 @@ class Board: public absBoard, public Subject {
   bool blindActivated = false;
   bool blockDroppedAfterBlind = false;
   public:
-  Board();
+  explicit Board(absBoard *board): board{board} {}
   void notify() override;
   char getState(int row, int col) const override;
   char tileAt(int row, int col) override;
