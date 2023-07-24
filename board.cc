03e0e8becde50bd41f9a852e6a85cdf31e39303c
stephen0403 <stephenim83@yahoo.ca>
2023-07-24 16:55:09 -0400

    Update board.cc

--- board.cc
+++ board.cc
@@ -6,7 +6,7 @@ Board::Board(absBoard *board): numRows{18}, numCols{11}, board(board) {
   shift(0, 0, false);
 }
 
-char Board::tileAt(int row, int col) { return board.at(row).at(col); }
+char Board::tileAt(int row, int col) { return board->at(row)->at(col); }
 
 Board::~Board() {}
 
