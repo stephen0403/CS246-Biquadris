2c9cc0cf66dead73809cd8c97328b399ce40790b
Alex Li <65035465+yook40@users.noreply.github.com>
2023-07-24 17:46:07 -0400

    Add files via upload

--- text.cc
+++ text.cc
@@ -1,7 +1,6 @@
 #include "text.h"
 #include "board.h"
 #include "block.h"
-#include "tile.h"
 #include <iostream>
 #include <sstream>
 
@@ -68,3 +67,69 @@ void TextDisplay::display() {
   }
 }
 */
+#include "gameplay.h"
+#include "text.h"
+#include "board.h"
+#include "block.h"
+#include <iostream>
+#include <sstream>
+
+TextDisplay::TextDisplay(GamePlay *gp): game{gp} {}
+
+static int numDigits(int num) {
+  if (!num) return 1;
+  int res = 0;
+  while (num) {
+    ++res;
+    num /= 10;
+  }
+  return res;
+}
+
+void TextDisplay::printHeader(std::string title, int num) const {
+  // std::ostringstream res;
+  std::cout << title;
+  int numSpaces = 5 - numDigits(num);
+  while (numSpaces) {
+    std::cout << " ";
+    --numSpaces;
+  }
+  std::cout << num;
+}
+
+void TextDisplay::display() {
+  std::string separateHeader{"        "};
+  std::string separateFooter{"                "};
+  std::string emptyNext{"                     "};
+  std::string border{"+-----------+"};
+  auto bp1 = game->boards[0].board;
+  auto bp2 = game->boards[1].board;
+  printHeader(" Level:", game->playerLevels[0]);
+  std::cout << separateHeader;
+  printHeader(" Level:", game->playerLevels[1]);
+  std::cout << std::endl;
+  printHeader(" Score:", game->playerScores[0]);
+  std::cout << separateHeader;
+  printHeader(" Score:", game->playerScores[1]);
+  std::cout << std::endl;
+  std::cout << border << separateHeader << border << std::endl;
+  for (int i = 0; i < 18; ++i) {
+    std::cout << '|';
+    for (auto c : bp1.at(i)) {
+      std::cout << c;
+    }
+    std::cout << '|' << separateHeader << '|';
+    for (auto c : bp2.at(i)) {
+      std::cout << c;
+    }
+    std::cout << '|' << std::endl;
+  }
+  std::cout << border << separateHeader << border << std::endl;
+  std::cout << " Next:" << separateFooter << "Next:" << std::endl;
+  if (game->curPlayer) {
+    std::cout << game->queue.at(1)->firstRow() << std::endl << game->queue.at(1)->secondRow() << std::endl;
+  } else {
+    std::cout << emptyNext << game->queue.at(1)->firstRow() << std::endl;
+    std::cout << emptyNext << game->queue.at(1)->secondRow() << std::endl;
+  }
+}
