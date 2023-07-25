c239b8863385093877de480d63d777e6ac58b565
stephen0403 <stephenim83@yahoo.ca>
2023-07-25 17:17:58 -0400

    Update absBoard.h

--- absBoard.h
+++ absBoard.h
@@ -14,8 +14,10 @@ class absBoard {
 public:
     virtual char tileAt(int row, int col) = 0;
     virtual ~absBoard();
-    virtual void rotateBlock(bool) = 0;
+    virtual void rotateBlock(Block *block, bool clockwise); = 0;
     virtual bool shift(int x, int y, Block *block, bool drop = false) = 0;
+    virtual void putBlock(int col, char type) = 0;
+    virtual bool clearRows() = 0;
 };
 
 #endif
