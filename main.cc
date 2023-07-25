7575fa66e55909526ae11ab6700f456f76b23fcd
danlu <daniel.lu2@uwaterloo.ca>
2023-07-25 11:19:22 -0400

    new main

--- main.cc
+++ main.cc
@@ -7,8 +7,23 @@
 int main(int argc, char *argv[]) {
   std::string file1{"sequence1.txt"};
   std::string file2{"sequence2.txt"};
-  int level = 0;
   unsigned int seed = 0;
+  int currentPlayer = 0;                                        // current player
+  
+  std::unique_ptr<absBoard> board1 = std::make_unique<Board> (); // board1
+  std::unique_ptr<absBoard> board2 = std::make_unique<Board> (); // board2
+  std::vector<absBoard*> boards {board1.get(), board2.get()};
+  std::vector<Level> levels{Level0(boards.at(currentPlayer)),
+                            Level1(boards.at(currentPlayer)),
+                            Level2(boards.at(currentPlayer)),
+                            Level3(boards.at(currentPlayer)),
+                            Level4(boards.at(currentPlayer))};
+  std::unique_ptr<Observer> textObserver = std::make_unique<TextDisplay>
+                                                (board1.get(), board2.get());
+  std::vector<int> playerLevels{0, 0}; //the levels each player is on
+  std::vector<Block> blocksQueue{}
+
+
   for (int i = 1; i < argc; i += 2) {
     std::string arg{argv[i]};
     if (arg == "-text") {
@@ -16,7 +31,7 @@ int main(int argc, char *argv[]) {
     } else {
       std::istringstream iss{argv[i + 1]};
       if (arg == "-startlevel") {
-        iss >> level;
+        iss >> playerLevels[currentPlayer];
       } else if (arg == "-scriptfile1") {
         iss >> file1;
       } else if (arg == "-scriptfile2") {
@@ -26,34 +41,27 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-  GamePlay game{file1, file2, 2, 2, seed};
-  TextDisplay td{&game};
-  std::string cmd;
-  td.display();
   std::cout << std::endl;
+/////////
+  std::string cmd;
   while (std::cin >> cmd) {
-    if (cmd == "d"/*rop"*/) {
-      try {
-        while(game.shift(0, 1, true));
-      } catch (bool player) {
-        std::cout << "Player " << player + 1 << " won!" << std::endl;
-        return 0;
-      }
-    } else if (cmd == "c"/*lockwise"*/) {
-      game.rotateBlock(1);
-    } else if (cmd == "cc"/*ounterclockwise"*/) {
-      game.rotateBlock(0);
-    } else if (cmd == "r"/*ight"*/) {
-      game.shift(1, 0);
-    } else if (cmd == "l"/*eft"*/) {
-      game.shift(-1, 0);
+    if (cmd == "left") {
+      boards[currentPlayer]->shift(-1, 0);
+    } else if (cmd == "right") {
     } else if (cmd == "down") {
-      game.shift(0, 1, false);
+    } else if (cmd == "clockwise") {
+    } else if (cmd == "counterclockwise") {
+    } else if (cmd == "drop") {
     } else if (cmd == "levelup") {
-      game.levelUp();
+      if (playerLevels[currentPlayer] < 4) ++playerLevels[currentPlayer];
     } else if (cmd == "leveldown") {
-      game.levelDown();
+      if (playerLevels[currentPlayer] > 0) --playerLevels[currentPlayer];
+    } else if (cmd == "norandom") { //needs further attention
+    } else if (cmd == "random") {
+    } else if (cmd == "sequence") { //needs further attention
+    } else if (cmd == "restart") {
     }
+    /*
     if (cmd == "d" || cmd == "r" || cmd == "l" || cmd == "down" || cmd == "c" || cmd == "cc") {
       try {
         game.notify();
@@ -67,6 +75,7 @@ int main(int argc, char *argv[]) {
     }
     td.display();
     std::cout << std::endl;
+    */
   }
 }
 
