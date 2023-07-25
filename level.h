6e8bdf99a1df7a2748d2956891d592c56065f3fc
Alex Li <65035465+yook40@users.noreply.github.com>
2023-07-25 18:29:49 -0400

    Update level.h

--- level.h
+++ level.h
@@ -1,16 +1,19 @@
 #include "block.h"
-#include "absBoard.h"
 #include <fstream>
 #include <vector>
 
+class absBoard;
 
 class Level {
   protected:
   absBoard *board;
+  bool random;
   public:
   Level(absBoard *);
   virtual std::unique_ptr<Block> newBlock() = 0;
   virtual void effect() const;
+  virtual void getFile(std::string);
+  virtual void setRandom(bool);
 };
 
 class Level0: public Level {
@@ -35,16 +38,23 @@ class Level2: public Level {
 };
 
 class Level3: public Level {
+  std::string name;
+  std::ifstream file;
   public:
   Level3(absBoard *);
   std::unique_ptr<Block> newBlock() override;
   void effect() const override;
+  void getFile(std::string) override;
+  void setRandom(bool) override;
 };
 
 class Level4: public Level {
+  std::string name;
+  std::ifstream file;
   public:
   Level4(absBoard *);
   std::unique_ptr<Block> newBlock() override;
   void effect() const override;
+  void getFile(std::string) override;
+  void setRandom(bool) override;
 };
-
