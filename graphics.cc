4d46db14762068e086052300b1852f6a881f5260
stephen0403 <stephenim83@yahoo.ca>
2023-07-28 14:19:11 -0400

    Update graphics.cc

--- graphics.cc
+++ graphics.cc
@@ -5,3 +5,9 @@ const int maxgrid = 500;
 GraphicObserver::GraphicObserver(Board *b1, Board *b2) : b1{b1}, b2{b2} {
   this->win.fillRectangle(0, 0, maxgrid, maxgrid, Xwindow::White);
 }
+
+
+void GraphicObserver::init() {
+
+
+}
