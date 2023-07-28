77b68b834c49264aded4554e9a737d0a687a5eec
stephen0403 <stephenim83@yahoo.ca>
2023-07-28 14:51:47 -0400

    Update blind.h

--- blind.h
+++ blind.h
@@ -10,6 +10,11 @@ class Blind: public Decorator {
     Blind(absBoard* board, char character);
     char tileAt(int row, int col) override;
     void setBlockDroppedTrue(); // this will set the blockDropped as true
+    bool isHeavy() override;
+    void rotateBlock(Block *block, bool clockwise) override;
+    bool shift(int x, int y, Block *block, bool drop = false) override;
+    void putBlock(int col, char type) override;
+    int clearRows() override;
 };
 
 #endif
