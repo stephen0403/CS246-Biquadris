78e8e1d9065c16ec77a08fe7c6330c04abc9f8eb
Alex Li <a394li@uwaterloo.ca>
2023-07-30 17:50:10 -0400

    added graphics class

---
+++ graphics.h
@@ -0,0 +1,15 @@
+#include "window.h"
+#include <memory>
+#include "cell.h"
+#include <vector>
+
+class GamePlay;
+
+class Graphics {
+  std::unique_ptr<Xwindow> window;
+  GamePlay *game;
+  std::vector<std::vector<Cell>> board[2];
+  public:
+  Graphics(GamePlay *);
+  void updateBoard();
+};
