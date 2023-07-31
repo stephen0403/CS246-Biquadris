53f48c5eb5de38eed134d1e794a991414b995865
stephen0403 <stephenim83@yahoo.ca>
2023-07-30 22:24:32 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -83,10 +83,10 @@ int main(int argc, char *argv[]) {
   bool readFromFile = false;
   std::ifstream file;
   int rowsCleared = 0;
-  std::vector<int> scores{0, 0};
-  std::unique_ptr<Graphics> graphicObserver = std::make_unique<Graphics>(board1.get(), board2.get(), playerLevels, currPlayer, scores, queueOfBlockQueues.at(0), queueOfBlockQueues.at(1));
-  int highScore = 0;
   bool blind = false;
+  std::vector<int> scores{0, 0};
+  std::unique_ptr<Graphics> graphicObserver = std::make_unique<Graphics>(board1.get(), board2.get(), playerLevels, currPlayer, scores, queueOfBlockQueues.at(0), queueOfBlockQueues.at(1), blind);
+  int highScore = 0;
   
 
   while (true) {
