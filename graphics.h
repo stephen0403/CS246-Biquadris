4042232bcc583ae9d8d4cfa2b9a8c506a5e25d7c
stephen0403 <stephenim83@yahoo.ca>
2023-07-30 22:23:13 -0400

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -15,7 +15,8 @@ class Graphics {
   std::vector<int> scores;
   std::vector<Block*> &queue1;
   std::vector<Block*> &queue2;
+  bool blind;
   public:
-  Graphics(Board* b1, Board* b2, std::vector<int> levels, int currPlayer, std::vector<int> scores, std::vector<Block*> &queue1, std::vector<Block*> &queue2);
+  Graphics(Board* b1, Board* b2, std::vector<int> levels, int currPlayer, std::vector<int> scores, std::vector<Block*> &queue1, std::vector<Block*> &queue2, bool blind);
   void updateBoard();
 };
